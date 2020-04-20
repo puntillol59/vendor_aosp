3b0eb3d8e7fe1505134192c49232d47f3d6f5b8f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-20 11:10:56 -0300

    vendor: Guard faceunlock
    Change-Id: I58aedaa815064ca9680d175befada87e187305ec

--- config/common.mk
+++ config/common.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
     Recorder
 
 # Face Unlock
+ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 TARGET_FACE_UNLOCK_SUPPORTED := false
 ifeq ($(TARGET_GAPPS_ARCH),arm64)
 ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
@@ -149,6 +150,7 @@ endif
 endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+endif
 
 # Branding
 include vendor/aosp/config/branding.mk
